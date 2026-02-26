06da63136ef7a870c7d9255f1e58f7f1d822314b
rukibhamz <53100544+rukibhamz@users.noreply.github.com>
2026-02-26 19:27:48 +0100

    Update sidebar-content.blade.php

--- resources/views/layouts/sidebar-content.blade.php
+++ resources/views/layouts/sidebar-content.blade.php
@@ -104,7 +104,7 @@
                 </a>
                 @endcan
                 @can('view employees')
-                <a href="{{ route('jobs.index') }}" class="group flex items-center px-2 py-2 text-sm font-medium rounded-md {{ request()->routeIs('jobs.*') ? 'sidebar-sub-link-active' : 'text-neutral-400 hover:text-white sidebar-nav-link-hover hover:bg-neutral-800' }}" :class="sidebarCollapsed ? 'justify-center' : ''">
+                <a href="{{ route('admin.jobs.index') }}" class="group flex items-center px-2 py-2 text-sm font-medium rounded-md {{ request()->routeIs('admin.jobs.*') ? 'sidebar-sub-link-active' : 'text-neutral-400 hover:text-white sidebar-nav-link-hover hover:bg-neutral-800' }}" :class="sidebarCollapsed ? 'justify-center' : ''">
                     <div class="w-6 flex justify-center" :class="sidebarCollapsed ? '' : 'mr-3'"><svg class="h-4 w-4 opacity-75 group-hover:opacity-100 transition-opacity" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/></svg></div>
                     <span x-show="!sidebarCollapsed" x-cloak>Jobs</span>
                 </a>
